e3cf3ea531f473b9a80ed81b65cecbb02042579f
Lorenzo Brasi <34858841+Lorenz94B@users.noreply.github.com>
2020-09-22 11:29:29 +0200

    Update script_mod.hpp

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -11,3 +11,6 @@
 #define COMPONENT_NAME QUOTE(FSCL - COMPONENT)
 
 #include "\x\cba\addons\main\script_macros_common.hpp"
+
+#undef PREP
+#define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
